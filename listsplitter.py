30a9d61a83af91a9c567f02b34ad92c3fdc4eb31
miko1034 <84585119+miko1034@users.noreply.github.com>
2024-04-14 13:30:43 +0100

    updated to not crash when invalid email

--- listsplitter.py
+++ listsplitter.py
@@ -1,18 +1,21 @@
 import os
 
-to_split = "10M_wattpad.txt"
+to_split = "3.9M(TWITTER-NETFLIX-FACEBOOK).txt"
 
 #will split the file into 3M segments
 
 newdir = f"{to_split}_fragmented"
 os.mkdir(newdir)
 
-with open(to_split, 'r') as f: 
-    count = 0 
+with open(to_split, 'r') as f:  
+    count = 0
     while True: 
         output_file = open(f'{newdir}/part{count}_{to_split}', 'w') 
-        for i in range(3000000): 
-            line = f.readline() 
+        for i in range(2000000):
+            try:
+                line = f.readline()
+            except UnicodeDecodeError:
+                print(f"{i}th email ignored") 
             if not line: 
                 output_file.close() 
                 break 
